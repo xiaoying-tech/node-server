78cc6d55f71214d0640cc4825e5addb6c8b71041
summer <943616879@qq.com>
2023-09-07 19:19:10 +0800

    fix: 添加前端包

---
+++ dist/server.js
@@ -0,0 +1,14 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const App = (0, express_1.default)();
+const PORT = 3000;
+App.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+App.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
--- server.ts
+++ server.ts
@@ -3,10 +3,13 @@ import express, { Request, Response, Application } from "express";
 const App: Application = express();
 const PORT = 3000;
 
-App.get("/", (req: Request, res: Response): void => {
+App.use(express.static(__dirname + "/static"));
+
+App.get("/data", (req: Request, res: Response): void => {
   res.send("Hello World!");
 });
 
 App.listen(PORT, (): void => {
   console.log(`Server is running on port ${PORT}`);
 });
+
