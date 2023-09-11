6a203493b3c2ed2e703f332b9faaa63d079ad519
summer <943616879@qq.com>
2023-09-11 19:05:32 +0800

    fix: 更改配置

--- dist/server.js
+++ dist/server.js
@@ -4,9 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
 const App = (0, express_1.default)();
 const PORT = 3000;
-App.use(express_1.default.static(__dirname + "/static"));
+App.use(express_1.default.static(path_1.default.join(__dirname, "static")));
 App.get("/data", (req, res) => {
     res.send("Hello World!");
 });
--- server.ts
+++ server.ts
@@ -1,9 +1,10 @@
 import express, { Request, Response, Application } from "express";
+import path from "path";
 
 const App: Application = express();
 const PORT = 3000;
 
-App.use(express.static(__dirname + "/static"));
+App.use(express.static(path.join(__dirname, "static")));
 
 App.get("/data", (req: Request, res: Response): void => {
   res.send("Hello World!");
