8d8aae22e6ee1ac1a2f8ecb231fddcec34d609d5
summer <943616879@qq.com>
2023-11-13 15:56:53 +0800

    fix: 修改静态文件的地址

--- dist/server.js
+++ dist/server.js
@@ -5,11 +5,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
+const fs_1 = __importDefault(require("fs"));
 const App = (0, express_1.default)();
 const PORT = 3000;
 App.use(express_1.default.static(path_1.default.join(__dirname, "static")));
 App.get("/data", (req, res) => {
     res.send("Hello World!");
+});
+App.use("/", function (req, res) {
+    res.setHeader("Content-Type", "text/html");
+    var html = fs_1.default.readFileSync(path_1.default.join(__dirname, "static/index.html"));
+    res.end(html);
 });
 App.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
--- server.ts
+++ server.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response, Application } from "express";
 import path from "path";
+import fs from "fs";
 
 const App: Application = express();
 const PORT = 3000;
@@ -8,6 +9,12 @@ App.use(express.static(path.join(__dirname, "static")));
 
 App.get("/data", (req: Request, res: Response): void => {
   res.send("Hello World!");
+});
+
+App.use("/", function (req, res) {
+  res.setHeader("Content-Type", "text/html");
+  var html = fs.readFileSync(path.join(__dirname, "static/index.html"));
+  res.end(html);
 });
 
 App.listen(PORT, (): void => {
