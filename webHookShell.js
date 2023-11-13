7ffc7c47e3f522e4e39579b712dce52221b85067
summer <943616879@qq.com>
2023-11-14 00:11:44 +0800

    fix: 添加运行shell脚本的js文件

---
+++ webHookShell.js
@@ -0,0 +1,18 @@
+const { exec } = require("child_process");
+
+// 指定要运行的脚本文件路径
+const scriptPath = "/webHookCallback.sh";
+
+// 使用 exec 函数运行脚本
+exec(`bash ${scriptPath}`, (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Error: ${error.message}`);
+    return;
+  }
+
+  // 打印脚本的标准输出
+  console.log(`stdout: ${stdout}`);
+
+  // 打印脚本的错误输出
+  console.error(`stderr: ${stderr}`);
+});
