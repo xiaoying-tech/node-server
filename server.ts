4484895e706741f36c49af1e6735e038bab1cd69
summer <943616879@qq.com>
2023-11-14 23:27:22 +0800

    fix: 测试代码

--- server.ts
+++ server.ts
@@ -3,18 +3,10 @@ import express, { Request, Response, Application } from "express";
 const App: Application = express();
 const PORT = 3000;
 
-// App.use(express.static(path.join(__dirname, "static")));
-
 App.get("/api/data", (req: Request, res: Response): void => {
   console.log(1111);
   res.send("Hello World!");
 });
-
-// App.use("/", function (req, res) {
-//   res.setHeader("Content-Type", "text/html");
-//   var html = fs.readFileSync(path.join(__dirname, "static/index.html"));
-//   res.end(html);
-// });
 
 App.listen(PORT, (): void => {
   console.log(`Server is running on port ${PORT}`);
