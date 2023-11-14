5dc89bc55dfa9a190684482c03b6f904af8b4707
summer <943616879@qq.com>
2023-11-14 22:58:25 +0800

    fix: 测试

--- server.ts
+++ server.ts
@@ -1,24 +1,22 @@
 import express, { Request, Response, Application } from "express";
-import path from "path";
-import fs from "fs";
 
 const App: Application = express();
 const PORT = 3000;
 
-App.use(express.static(path.join(__dirname, "static")));
+// App.use(express.static(path.join(__dirname, "static")));
 
 App.get("/api/data", (req: Request, res: Response): void => {
+  console.log(1111);
   res.send("Hello World!");
 });
 
-App.use("/", function (req, res) {
-  res.setHeader("Content-Type", "text/html");
-  var html = fs.readFileSync(path.join(__dirname, "static/index.html"));
-  res.end(html);
-});
+// App.use("/", function (req, res) {
+//   res.setHeader("Content-Type", "text/html");
+//   var html = fs.readFileSync(path.join(__dirname, "static/index.html"));
+//   res.end(html);
+// });
 
 App.listen(PORT, (): void => {
   console.log(`Server is running on port ${PORT}`);
   console.log("测试一下，是否可以自动化拉代码！！！");
 });
-
