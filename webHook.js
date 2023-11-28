0d252a17981acdd9b2443cbe51b48481d707f76a
summer <943616879@qq.com>
2023-11-28 15:36:06 +0800

    fix: 去掉不要的文件

--- webHook.js
+++ webHook.js
@@ -8,7 +8,7 @@ const port = 1001; //
 app.use(bodyParser.json()); // 解析 JSON 请求体
 
 // 指定要运行的脚本文件路径
-const scriptPath = "webHookCallback.sh";
+const scriptPath = "webhookCallback.sh";
 const execCallback = () => {
   // 使用 exec 函数运行脚本
   exec(`bash ${scriptPath}`, (error, stdout, stderr) => {
@@ -26,7 +26,7 @@ const execCallback = () => {
 };
 
 // 处理 Webhook 请求
-app.post("/webhook", (req, res) => {
+app.post("/backEndWebhook", (req, res) => {
   const payload = req.body;
   // 处理 Webhook 收到的数据，根据你的需求进行相应的处理
 
@@ -36,7 +36,7 @@ app.post("/webhook", (req, res) => {
   res.status(200).send("webhook消息成功解析");
 });
 
-app.get("/sendData", (req, res) => {
+app.get("/backEndData", (req, res) => {
   console.log("接口测试");
 
   res.status(200).send("hello webhook");
